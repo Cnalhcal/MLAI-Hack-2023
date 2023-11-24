8245dea7558375c310548cb8d5265cf3bdadff9e
Sadiah Z <100665526+SadiahZ14@users.noreply.github.com>
2023-11-25 09:39:12 +1100

    Update sad_dataset.py

--- sad_dataset.py
+++ sad_dataset.py
@@ -2,6 +2,45 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+
+def calculate_fire_spread_rate(wind_speed_at_10m, moisture_content, slope_angle, fuel_load=2):
+    """
+    Calculate the rate of spread of a forest fire.
+
+    Parameters:
+    wind_speed_at_10m (float): Wind speed at 10 meters height in km/hr.
+    moisture_content (float): Moisture content in percentage (35% - 65%).
+    slope_angle (float): Slope angle in degrees.
+    fuel_load (float): Fuel load in tonnes/hectare (< 6 mm diameter), default is 2 tonnes/hectare.
+
+    Returns:
+    float: Rate of spread of the fire in unspecified units.
+    """
+    # Constants
+    a = 1.674
+    b = 0.1798
+    c = 0.22
+    d = 0.158
+    e = -0.227
+    f = 0.0662
+
+    # Convert wind speed at 10m to wind speed at 1.5m
+    wind_speed_at_1_5m = a + b * wind_speed_at_10m
+
+    # Calculate the rate of spread on flat ground
+    rate_flat_ground = c * fuel_load * (wind_speed_at_1_5m ** d) * (moisture_content ** e)
+
+    # Adjust rate for slope
+    rate_on_slope = rate_flat_ground * (slope_angle ** f)
+
+    return rate_on_slope
+
+# Example usage
+spread_rate = calculate_fire_spread_rate(20, 50, 10)
+spread_rate
+
+#################################################################################################
+
 def generate_features():
     features = {
         'fuel_age': np.random.uniform(0, 30),
